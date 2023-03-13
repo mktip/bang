28d5bf9851934c14249d15bf6fdbe413a428d6a9
Mohammad Issa <mo.issa.ok@gmail.com>
2023-03-13 09:32:11 +0300

    add cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,14 @@
-#![allow(unused_variables)]
-
-// import the generated parser
 use bang::eval::{eval_prgm, RunErr, Value};
 use bang::parse::BangParser;
-
-// get filename from  args and evaluate it with eval_prgm after parsing it
-// with BangParser
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     let filename = args[1].clone();
+
     let txt = std::fs::read_to_string(filename).unwrap();
 
     let val = match BangParser::parse(txt.as_str()) {
-        Ok(node) => {
-            dbg!(&node);
-            eval_prgm(node)
-        }
+        Ok(node) => eval_prgm(node),
         Err(e) => panic!("Error Parsing: {:?}", e),
     };
 
